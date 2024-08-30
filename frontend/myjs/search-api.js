1f11241f58f87ed9ba282a66db646878af586fa7
Fo4tun3 <forfortify4114@gmail.com>
2024-08-30 05:38:03 +0100

    added render link

--- frontend/myjs/search-api.js
+++ frontend/myjs/search-api.js
@@ -21,7 +21,7 @@ document.getElementById('detectionForm').addEventListener('submit', function(eve
     event.preventDefault();
     const inputText = document.getElementById('inputText').value;
     
-    fetch('http://127.0.0.1:8080/api/v1/predict-sqli-xss', {
+    fetch('https://sqli-xss-attack-detection-api.onrender.com/api/v1/predict-sqli-xss', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
