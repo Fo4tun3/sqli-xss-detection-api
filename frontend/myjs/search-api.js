0c73e985598fad06c445236dc92010065fff6f1a
Fo4tun3 <forfortify4114@gmail.com>
2024-08-30 05:03:06 +0100

    updated the search page

--- frontend/myjs/search-api.js
+++ frontend/myjs/search-api.js
@@ -31,13 +31,38 @@ document.getElementById('detectionForm').addEventListener('submit', function(eve
     .then(response => response.json())
     .then(data => {
         const resultDiv = document.getElementById('result');
+        // const resultInput = document.getElementById('resultInput');
         
+        const sql = data.prediction['SQL Injection'];
+        const xss = data.prediction['Cross-Site Scripting (XSS)'];
+        const nom = data.prediction['Normal'];
+
+        let largestNumber = Math.max(sql, xss, nom);
+        let predictedPercent = (largestNumber * 100).toFixed(3) + '%';
+        
+        // if (data.predicted_class == 'Normal') {
+        //     resultInput.style.backgroundColor = 'green';
+        // }
+
         resultDiv.innerHTML = `
             <h2>Prediction Result</h2>
-            <h5><strong>SQL Injection:</strong> ${data.prediction['SQL Injection']}</h5>
+
+            <div class="new_predict_result">
+                <div>
+                    <label for="predicted_class" style="font-weight: bold;">Predicted Class:
+                        <input id= "resultInput" class="resultInput" type="text" disabled value="${data.predicted_class}">
+                    </label>
+                </div>
+                <div>
+                    <label for="predicted_percentage" style="font-weight: bold;">Prediction Percent:
+                        <input id= "resultInput" class="resultInput" type="text" disabled value="${predictedPercent}">
+                    </label>
+                </div>
+            </div>
+            <!-- <h5><strong>SQL Injection:</strong> ${data.prediction['SQL Injection']}</h5>
             <h5><strong>Cross-Site Scripting (XSS):</strong> ${data.prediction['Cross-Site Scripting (XSS)']}</h5>
             <h5><strong>Normal:</strong> ${data.prediction['Normal']}</h5>
-            <h5><strong>Predicted Class:</strong> ${data.predicted_class}</h5>
+            <h5><strong>Predicted Class:</strong> ${data.predicted_class}</h5> --!>
         `;
     })
     .catch(error => {
